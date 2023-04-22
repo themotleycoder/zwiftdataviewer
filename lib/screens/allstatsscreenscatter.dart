5fd33a951c540f70149e26057387376546b39a3f
sharne <sharne@MacBook-Air-3.local>
2023-04-22 21:30:17 +0200

    fixed scatter chart

--- lib/screens/allstatsscreenscatter.dart
+++ lib/screens/allstatsscreenscatter.dart
@@ -1,4 +1,3 @@
-import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'package:flutter_palette/flutter_palette.dart';
 import 'package:provider/provider.dart';
@@ -24,8 +23,6 @@ class _AllStatsScreenScatterState extends State<AllStatsScreenScatter> {
         selector: (_, model) => model.dateFilteredActivities,
         builder: (context, activities, child) {
           Map<String, String> units = Conversions.units(context);
-          final List<charts.Series<dynamic, double>> seriesList =
-              generateChartData(context, units, activities);
           return Column(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -35,21 +32,6 @@ class _AllStatsScreenScatterState extends State<AllStatsScreenScatter> {
                     child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: buildScatterChart(context, units, activities),
-                )),
-                Expanded(
-                    child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: charts.ScatterPlotChart(
-                    seriesList,
-                    animate: true,
-                    behaviors: [charts.SeriesLegend()],
-                    selectionModels: [
-                      charts.SelectionModelConfig(
-                        type: charts.SelectionModelType.info,
-                        changedListener: _onSelectionChanged,
-                      )
-                    ],
-                  ),
                 )),
                 Consumer<SummaryActivitySelectDataModel>(
                     builder: (context, summaryActivity, child) {
@@ -88,41 +70,14 @@ class _AllStatsScreenScatterState extends State<AllStatsScreenScatter> {
         });
   }
 
-  List<charts.Series<SummaryActivity, double>> generateChartData(
-      BuildContext? context,
-      Map<String, String> units,
-      List<SummaryActivity> activities) {
-    /// Create series list with multiple series
-    final Map<int, List<SummaryActivity>> result = {};
-    final List<int> years = [];
-
-    for (var activity in activities) {
-      int year = activity.startDateLocal!.year;
-      if (!result.containsKey(year)) {
-        result[year] = [];
-      }
-      if (!years.contains(year)) {
-        years.add(year);
-      }
-
-      result[year]!.add(activity);
-    }
-
-    final Map<int, Color> colors = generateColor(years);
-
-    return ChartsData.buildChartSeriesList(context!, result, colors);
-  }
-
-  _onSelectionChanged(charts.SelectionModel model) {
-    int? selection = model.selectedDatum[0].index ?? 0;
-    final SummaryActivity selectedRide =
-        model.selectedSeries[0].data[selection];
-    Provider.of<SummaryActivitySelectDataModel>(context, listen: false)
-        .setSelectedActivity(selectedRide);
-  }
-
   SfCartesianChart buildScatterChart(
       BuildContext context, units, List<SummaryActivity> activities) {
+
+    final Map<int, List<SummaryActivity>> result =
+    groupActivitiesByYear(activities);
+
+    final chartSeries = getScatterSeries(context, units, result);
+
     return SfCartesianChart(
       primaryXAxis: NumericAxis(),
       primaryYAxis: NumericAxis(
@@ -131,53 +86,34 @@ class _AllStatsScreenScatterState extends State<AllStatsScreenScatter> {
         labelFormat: '{value}',
         minimum: 0,
       ),
-      series: getScatterSeries(context, units, activities),
+      series: chartSeries,
       legend: Legend(
         isVisible: true,
         position: LegendPosition.top,
-        // offset: enableFloatingLegend ? Offset(_xOffset, _yOffset) : null,
-        // overflowMode: _overflow      Mode,
-        // toggleSeriesVisibility: toggleVisibility,
-        // backgroundColor: model.currentThemeData?.brightness == Brightness.light
-        //     ? Colors.white.withOpacity(0.5)
-        //     : const Color.fromRGBO(33, 33, 33, 0.5),
-        // borderColor: model.currentThemeData?.brightness == Brightness.light
-        //     ? Colors.black.withOpacity(0.5)
-        //     : Colors.white.withOpacity(0.5),
         borderWidth: 1,
       ),
     );
   }
 
-  List<charts.Series<SummaryActivity, double>> getScatterSeries(
-      BuildContext context, units, List<SummaryActivity> activities) {
-    // List<charts.Series<SummaryActivity, double>> series = [];
-    // var chartData = generateChartData(context, units, activities);
-
-    final Map<int, List<SummaryActivity>> result =
-        groupActivitiesByYear(activities);
-    final List<int> years = result.keys.toList();
-    final List<charts.Series<SummaryActivity, double>> chartSeries = [];
+  List<ChartSeries<dynamic, dynamic>> getScatterSeries(
+      BuildContext context, units, Map<int, List<SummaryActivity>> activities) {
+    final List<int> years = activities.keys.toList();
+    final List<ChartSeries<dynamic, dynamic>> chartSeries = [];
 
     final Map<int, Color> colors = generateColor(years);
 
-    for (int key in result.keys) {
-      final List<SummaryActivity> distance = result[key]!;
-      chartSeries.add(charts.Series<SummaryActivity, double>(
-        id: key.toString().substring(2),
-        // Providing a color function is optional.
-        colorFn: (SummaryActivity stats, _) {
-          // Bucket the measure column value into 3 distinct colors.
-          return charts.ColorUtil.fromDartColor(
-              colors[stats.startDateLocal!.year]!);
+    for (int key in colors.keys) {
+      chartSeries.add(ScatterSeries<SummaryActivity, double>(
+        name: key.toString().substring(2),
+        color: colors[key], // Set the color property for the series
+        pointColorMapper: (SummaryActivity stats, _) {
+          return colors[stats.startDateLocal!.year]!;
         },
-        domainFn: (SummaryActivity stats, _) =>
+        xValueMapper: (SummaryActivity stats, _) =>
             Conversions.metersToDistance(context, stats.distance ?? 0),
-        measureFn: (SummaryActivity stats, _) =>
+        yValueMapper: (SummaryActivity stats, _) =>
             Conversions.metersToHeight(context, stats.totalElevationGain ?? 0),
-        // Providing a radius function is optional.
-        // radiusPxFn: (SummaryActivity stats, _) => sales.radius,
-        data: distance,
+        dataSource: activities[key]!,
       ));
     }
     return chartSeries;
@@ -200,7 +136,11 @@ class _AllStatsScreenScatterState extends State<AllStatsScreenScatter> {
 }
 
 Map<int, Color> generateColor(List<int> years) {
-  ColorPalette palette = ColorPalette.splitComplimentary(
+  if (years == null || years.isEmpty) {
+    return {};
+  }
+
+  final ColorPalette palette = ColorPalette.splitComplimentary(
     zdvMidGreen,
     numberOfColors: years.length,
     hueVariability: 30,
@@ -208,12 +148,5 @@ Map<int, Color> generateColor(List<int> years) {
     brightnessVariability: 30,
   );
 
-  int x = 0;
-  final Map<int, Color> colors = {};
-  for (Color color in palette) {
-    colors[years[x]] = color;
-    x += 1;
-  }
-
-  return colors;
+  return { for (var entry in years.asMap().entries) entry.value : palette[entry.key] };
 }
